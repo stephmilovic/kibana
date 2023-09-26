bc6c2a2dcb95ac781ee1ddcdb1cfeac0d882ee47
Steph Milovic <stephanie.milovic@elastic.co>
2023-09-26 12:02:58 -0600

    more

--- x-pack/examples/gen_ai_streaming_response_example/public/components/setup_connector.tsx
+++ x-pack/examples/gen_ai_streaming_response_example/public/components/setup_connector.tsx
@@ -6,7 +6,7 @@
  */
 
 import React from 'react';
-import { GenAiLogo } from '@kbn/stack-connectors-plugin/public/common';
+import { OpenAILogo } from '@kbn/stack-connectors-plugin/public/common';
 import { EuiFlexGroup, EuiCard, EuiFlexItem, EuiIcon } from '@elastic/eui';
 import { i18n } from '@kbn/i18n';
 
@@ -20,7 +20,7 @@ export const SetupConnector = ({ setIsConnectorModalVisible }: SetupConnectorPro
       <EuiFlexItem grow={false}>
         <EuiCard
           layout="horizontal"
-          icon={<EuiIcon size="xl" type={GenAiLogo} />}
+          icon={<EuiIcon size="xl" type={OpenAILogo} />}
           title={i18n.translate(
             'genAiStreamingResponseExample.app.component.addConnectorCardTitle',
             {
