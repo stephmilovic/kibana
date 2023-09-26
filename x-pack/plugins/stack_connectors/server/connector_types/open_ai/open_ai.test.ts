f869286f736dbb94dda6e319968ab9586f558c1a
Steph Milovic <stephanie.milovic@elastic.co>
2023-09-26 16:04:21 -0600

    improves error handling

--- x-pack/plugins/stack_connectors/server/connector_types/open_ai/open_ai.test.ts
+++ x-pack/plugins/stack_connectors/server/connector_types/open_ai/open_ai.test.ts
@@ -16,6 +16,7 @@ import { loggingSystemMock } from '@kbn/core-logging-server-mocks';
 import { actionsMock } from '@kbn/actions-plugin/server/mocks';
 import { RunActionResponseSchema, StreamingResponseSchema } from '../../../common/open_ai/schema';
 import { initDashboard } from './create_dashboard';
+import { AxiosError } from 'axios';
 jest.mock('./create_dashboard');
 
 describe('OpenAIConnector', () => {
@@ -280,6 +281,61 @@ describe('OpenAIConnector', () => {
         connector.request = mockError;
 
         await expect(connector.invokeAI(sampleOpenAiBody)).rejects.toThrow('API Error');
+      });
+    });
+
+    describe('getResponseErrorMessage', () => {
+      it('returns an unknown error message', () => {
+        // @ts-expect-error expects an axios error as the parameter
+        expect(connector.getResponseErrorMessage({})).toEqual(
+          `Unexpected API Error:  - Unknown error`
+        );
+      });
+
+      it('returns the error.message', () => {
+        // @ts-expect-error expects an axios error as the parameter
+        expect(connector.getResponseErrorMessage({ message: 'a message' })).toEqual(
+          `Unexpected API Error:  - a message`
+        );
+      });
+
+      it('returns the error.response.data.error.message', () => {
+        const err = {
+          response: {
+            headers: {},
+            status: 404,
+            statusText: 'Resource Not Found',
+            data: {
+              error: {
+                message: 'Resource not found',
+              },
+            },
+          },
+        } as AxiosError<{ error?: { message?: string } }>;
+        expect(
+          // @ts-expect-error expects an axios error as the parameter
+          connector.getResponseErrorMessage(err)
+        ).toEqual(`API Error: Resource Not Found - Resource not found`);
+      });
+
+      it('returns auhtorization error', () => {
+        const err = {
+          response: {
+            headers: {},
+            status: 401,
+            statusText: 'Auth error',
+            data: {
+              error: {
+                message: 'The api key was invalid.',
+              },
+            },
+          },
+        } as AxiosError<{ error?: { message?: string } }>;
+
+        // @ts-expect-error expects an axios error as the parameter
+        expect(connector.getResponseErrorMessage(err)).toEqual(
+          `Unauthorized API Error - The api key was invalid.`
+        );
       });
     });
   });
