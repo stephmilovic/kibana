c2df83fc205aea09a4963a6179d653477c805f69
Steph Milovic <stephanie.milovic@elastic.co>
2024-07-10 12:55:00 -0600

    wip again

--- x-pack/packages/kbn-langchain/server/language_models/simple_chat_model.ts
+++ x-pack/packages/kbn-langchain/server/language_models/simple_chat_model.ts
@@ -11,15 +11,17 @@ import {
 } from '@langchain/core/language_models/chat_models';
 import { ReadableStream } from 'web-streams-polyfill';
 import type { ActionsClient } from '@kbn/actions-plugin/server';
-import { AIMessageChunk, BaseMessage } from '@langchain/core/messages';
+import { BaseMessage } from '@langchain/core/messages';
 import { ChatGenerationChunk } from '@langchain/core/outputs';
 import { Logger } from '@kbn/logging';
 import { v4 as uuidv4 } from 'uuid';
 import { get } from 'lodash/fp';
 import { CallbackManagerForLLMRun } from '@langchain/core/callbacks/manager';
 import { PublicMethodsOf } from '@kbn/utility-types';
-// import { IterableReadableStream } from '@langchain/core/dist/utils/stream';
 import { Readable } from 'stream';
+import { parseGeminiStream } from '../utils/gemini';
+import { parseBedrockStream } from '../utils/bedrock';
+// import { IterableReadableStream } from '@langchain/core/dist/utils/stream';
 import { parseBedrockIterable } from '..';
 import { getDefaultArguments } from './constants';
 
@@ -106,40 +108,98 @@ export class ActionsClientSimpleChatModel extends SimpleChatModel {
         llmType: this.llmType,
         stop: options.stop,
       });
-      const stream = await this.caller.call(async () =>
-        createSimpleChatStream({
-          requestBody,
-          actionsClient: this.#actionsClient,
-          llmType: this.llmType,
-          logger: this.#logger,
-          runManager,
-          signal: this.#signal,
-        })
-      );
-      for await (const chunk of stream) {
-        console.log('stephhh CHUNK OF STREAM', chunk);
-        if (!chunk.done) {
-          yield new ChatGenerationChunk({
-            text: chunk.message.content,
-            message: new AIMessageChunk({ content: chunk.message.content }),
-          });
-          await runManager?.handleLLMNewToken(chunk.message.content ?? '');
-        } else {
-          yield new ChatGenerationChunk({
-            text: '',
-            message: new AIMessageChunk({ content: '' }),
-            // generationInfo: {
-            //   model: chunk.model,
-            //   total_duration: chunk.total_duration,
-            //   load_duration: chunk.load_duration,
-            //   prompt_eval_count: chunk.prompt_eval_count,
-            //   prompt_eval_duration: chunk.prompt_eval_duration,
-            //   eval_count: chunk.eval_count,
-            //   eval_duration: chunk.eval_duration,
-            // },
-          });
-        }
+
+      const actionResult = await this.#actionsClient.execute(requestBody);
+
+      if (actionResult.status === 'error') {
+        throw new Error(
+          `ActionsClientSimpleChatModel: action result status is error: ${actionResult?.message} - ${actionResult?.serviceMessage}`
+        );
       }
+
+      const readable = get('data', actionResult) as Readable;
+
+      if (typeof readable?.read !== 'function') {
+        throw new Error('Action result status is error: result is not streamable');
+      }
+
+      let currentOutput = '';
+      let finalOutputIndex = -1;
+      const finalOutputStartToken = '"action":"FinalAnswer","action_input":"';
+      let streamingFinished = false;
+      const finalOutputStopRegex = /(?<!\\)\"/;
+      let extraOutput = '';
+      const handleLLMNewToken = async function* (token: string) {
+        if (finalOutputIndex === -1) {
+          // Remove whitespace to simplify parsing
+          currentOutput += token.replace(/\s/g, '');
+          if (currentOutput.includes(finalOutputStartToken)) {
+            finalOutputIndex = currentOutput.indexOf(finalOutputStartToken);
+            const contentStartIndex = finalOutputIndex + finalOutputStartToken.length;
+            extraOutput = currentOutput.substring(contentStartIndex);
+          }
+        } else if (!streamingFinished) {
+          const finalOutputEndIndex = token.search(finalOutputStopRegex);
+          if (finalOutputEndIndex !== -1) {
+            extraOutput = token.substring(0, finalOutputEndIndex);
+            streamingFinished = true;
+            if (extraOutput.length > 0) {
+              yield {
+                done: true,
+                message: { content: extraOutput },
+              };
+              await runManager?.handleLLMNewToken(extraOutput);
+            }
+          } else {
+            const tokenOutput = `${extraOutput}${token}`;
+            extraOutput = '';
+            yield {
+              done: false,
+              message: { content: tokenOutput },
+            };
+            await runManager?.handleLLMNewToken(tokenOutput);
+          }
+        }
+      };
+      const streamParser = this.llmType === 'bedrock' ? parseBedrockStream : parseGeminiStream;
+
+      const parsed = void streamParser(readable, this.#logger, this.#signal, handleLLMNewToken);
+      yield* handleLLMNewToken;
+      // return parsed; // per the contact of _call, return a string
+      // const stream = await this.caller.call(async () =>
+      //   createSimpleChatStream({
+      //     requestBody,
+      //     actionsClient: this.#actionsClient,
+      //     llmType: this.llmType,
+      //     logger: this.#logger,
+      //     runManager,
+      //     signal: this.#signal,
+      //   })
+      // );
+      // for await (const chunk of stream) {
+      //   console.log('stephhh CHUNK OF STREAM', chunk);
+      //   if (!chunk.done) {
+      //     yield new ChatGenerationChunk({
+      //       text: chunk.message.content,
+      //       message: new AIMessageChunk({ content: chunk.message.content }),
+      //     });
+      //     await runManager?.handleLLMNewToken(chunk.message.content ?? '');
+      //   } else {
+      //     yield new ChatGenerationChunk({
+      //       text: '',
+      //       message: new AIMessageChunk({ content: '' }),
+      //       // generationInfo: {
+      //       //   model: chunk.model,
+      //       //   total_duration: chunk.total_duration,
+      //       //   load_duration: chunk.load_duration,
+      //       //   prompt_eval_count: chunk.prompt_eval_count,
+      //       //   prompt_eval_duration: chunk.prompt_eval_duration,
+      //       //   eval_count: chunk.eval_count,
+      //       //   eval_duration: chunk.eval_duration,
+      //       // },
+      //     });
+      //   }
+      // }
       // eslint-disable-next-line @typescript-eslint/no-explicit-any
     } catch (e: any) {
       throw e;
